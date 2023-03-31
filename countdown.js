cd0220c08fd843a9c4b4e358fc69841a5a4546f9
abhishek9392 <abhishekreddy581@gmail.com>
2023-03-31 20:45:29 +0530

    two

---
+++ countdown.js
@@ -0,0 +1,19 @@
+// jeegrpnerh
+var count="0"
+function add(){
+    count++
+    var b=document.getElementById("inp1")
+    b.value=count
+}
+function del(){
+    if(count>=1){
+        count--
+
+    var c=document.getElementById("inp1")
+    c.value=count
+    }
+    else{
+
+    }
+}         
+
